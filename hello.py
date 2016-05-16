05ec51f1550120f3f734d7961c9c833f4c8d2c28
Illuzzion <cahek1@inbox.ru>
2016-05-16 23:09:26 +0300

    second v1

---
+++ hello.py
@@ -0,0 +1,17 @@
+from cgi import parse_qs
+
+#gunicorn -b 0.0.0.0:8080 hello:app
+
+def app(environ, start_response):
+
+	query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	body = []
+	for key, values in query.items():
+		for item in values:
+			body.append(key + "=" + item + "\r\n")
+   
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain')]
+  
+	start_response(status, headers)
+	return body
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
+sudo gunicorn hello:app &
