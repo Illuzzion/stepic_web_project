3c983cfc739f902b3bb6ea47e50aa92fe62a7b88
Illuzzion <79fz270704@gmail.com>
2016-05-18 08:48:48 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo gunicorn -b 0.0.0.0:8080 hello:app &
